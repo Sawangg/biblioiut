f2276db478be10dcdffbf5d3c16a8f37123cc263
Apokalypt <58733915+Apokalypt@users.noreply.github.com>
2020-01-06 15:29:32 +0100

    Delete Color.cpp

--- Color.cpp
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-
-void SetColorAndBackground(int ForgC, int BackC) {
-	WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
-	return;
-}
